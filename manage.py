2cc4ff9889d437041e6db3f1a2f988f2c0734e39
Koko-66 <k.cisek@gmail.com>
2022-03-24 22:50:18 +0000

    Update files to reflect new project name.

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    settings = 'eshop.test_settings' if 'test' in sys.argv else 'eshop.settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eshop.settings')
+    settings = 'green_bean.test_settings' if 'test' in sys.argv else 'green_bean.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'green_bean.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
