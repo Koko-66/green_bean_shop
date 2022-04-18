135f6985bb475249428bb0947c5eef0633c8ed20
Koko-66 <k.cisek@gmail.com>
2022-04-18 21:47:28 +0000

    Add functionality to adjust and remove items from bag.

--- bag/urls.py
+++ bag/urls.py
@@ -6,5 +6,7 @@ from bag import views
 app_name = 'bag'
 urlpatterns = [
     path('', views.ViewBag.as_view(), name='view_bag'),
-    path('add/<int:item_id>/', views.add_to_bag, name='add_to_bag')
+    path('add/<int:item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('adjust/<int:item_id>/', views.adjust_bag, name='adjust_bag'),
+    path('remove_item/<int:item_id>/', views.remove_item, name='remove_item'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,10 @@
 
 """Views for 'bag' app"""
-from django.shortcuts import redirect
+from django.shortcuts import (
+    redirect,
+    reverse,
+    HttpResponse,
+)
 from django.views.generic import TemplateView
 
 
@@ -71,5 +75,107 @@ def add_to_bag(request, item_id):
 
     # overwrite bag in the session with updated one
     request.session['bag'] = bag
-    print(request.session['bag'])
+
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust items in the bag"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    color = None
+    if 'size' in request.POST:
+        size = request.POST['size']
+    if 'color' in request.POST:
+        color = request.POST['color']
+    bag = request.session.get('bag', {})
+    print(bag)
+    str_item_id = str(item_id)
+    # Handle different types of products depending on whether they have size
+    # or color, both or none.
+    if size:
+        if color:
+            if quantity > 0:
+                bag[str_item_id]['items_by_size'][
+                    size]['items_by_color'][color] = quantity
+            else:
+                del bag[str_item_id]['items_by_size'][size][
+                        'items_by_color'][color]
+                if not bag[str_item_id]['items_by_size'][size][
+                        'items_by_color']:
+                    bag.pop(str_item_id)
+        else:
+            if quantity > 0:
+                bag[str_item_id]['items_by_size'][size] = quantity
+            else:
+                del bag[str_item_id]['items_by_size'][size]
+                if not bag[str_item_id]['items_by_size']:
+                    bag.pop(str_item_id)
+    # Handling products without size
+    else:
+        if color:
+            if quantity > 0:
+                bag[str_item_id]['items_by_color'][color] = quantity
+            else:
+                del bag[str_item_id]['items_by_color'][color]
+                if not bag[str_item_id]['items_by_color']:
+                    bag.pop(str_item_id)
+        else:
+            print('code reached here')
+            if quantity > 0:
+                bag[str_item_id] = quantity
+            else:
+                bag.pop(str_item_id)
+
+    # overwrite bag in the session with updated one
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(reverse('bag:view_bag'))
+
+
+def remove_item(request, item_id):
+    """Remove item from the bag"""
+
+    try:
+        size = None
+        color = None
+        if 'size' in request.POST:
+            size = request.POST['size']
+        if 'color' in request.POST:
+            color = request.POST['color']
+        bag = request.session.get('bag', {})
+        print(bag)
+
+        str_item_id = str(item_id)
+        # Handle different types of products depending on whether they
+        # have size or color, both or none.
+        if size:
+            if color:
+                del bag[str_item_id]['items_by_size'][size][
+                    'items_by_color'][color]
+                if not bag[str_item_id]['items_by_size'][size][
+                        'items_by_color']:
+                    bag.pop(str_item_id)
+                print(f'Bag after remvoving item {bag}')
+            else:
+                del bag[str_item_id]['items_by_size'][size]
+                if not bag[str_item_id]['items_by_size']:
+                    bag.pop(str_item_id)
+
+        # Handling products without size
+        else:
+            if color:
+                del bag[str_item_id]['items_by_color'][color]
+                if not bag[str_item_id]['items_by_color']:
+                    bag.pop(str_item_id)
+            else:
+                bag.pop(str_item_id)
+
+        request.session['bag'] = bag
+        print(request.session['bag'])
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        print(e)
+        return HttpResponse(status=500)
