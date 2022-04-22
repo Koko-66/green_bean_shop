8bd6faa64eb6a9db5074d67ebf5cda2b76bb9211
Koko-66 <k.cisek@gmail.com>
2022-04-22 16:51:39 +0000

    Add admin, singals and form to checkout app.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderLineItem
+
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """Register OrderLine in the admin and show in Order as inline."""
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """Register Order in the admin and show inlines."""
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
+
+    fields = ('order_number', 'user_profile', 'date',   'full_name',
+              'email', 'street_address1',
+              'street_address2', 'town_or_city', 'county', 'postcode', 
+              'country',
+              'phone_number',
+              'delivery_cost',
+              'order_total', 'grand_total')
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
+
