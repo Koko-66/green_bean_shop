127f366efe2ef951ca14b346db03f95f6c0e1944
Koko-66 <k.cisek@gmail.com>
2022-05-01 02:21:44 +0000

    Fix typo in form.submit().

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -107,7 +107,7 @@ form.addEventListener('submit', function(ev) {
                 $('#submit-button').attr('disabled', false);
             } else {
                 if (result.paymentIntent.status === 'succeeded') {
-                    form÷.submit();
+                    form.submit();
                 }
             }
         });
