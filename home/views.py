23b672a8fd0ef98c8fab61aad182bf317c01522b
Koko-66 <k.cisek@gmail.com>
2022-05-10 16:04:10 +0000

    Remove commented out code.

--- home/views.py
+++ home/views.py
@@ -7,5 +7,3 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     """Render home view"""
     template_name = 'home/index.html'
-    # success_message = 'Home page rendered successfully'
-    # print(success_message)
