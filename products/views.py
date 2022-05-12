82fb549480c8d5e3f7df289bf12ade024bfb5ec6
Koko-66 <k.cisek@gmail.com>
2022-05-12 17:58:11 +0000

    Update product filters to handle sorting.

--- products/views.py
+++ products/views.py
@@ -15,7 +15,6 @@ from django.views.generic import (
     DeleteView,
     CreateView,
 )
-
 from .models import (
     Category,
     Product,
@@ -38,50 +37,78 @@ class ProductListView(ListView):
         query = None
         sort = None
         direction = None
+        colors = None
+        sizes = None
 
         if 'sort' in self.request.GET:
             sortkey = self.request.GET['sort']
             sort = sortkey
-            if sortkey == 'product_name':
-                sortkey = 'lower_name'
-                products = products.annotate(
+            print(sortkey)
+            # if sortkey == 'rating':
+            #     products = products.annotate(
+            #         rating='rating'
+            #     )
+
+            if sortkey == 'name-asc':
+               sortkey = 'product_name'
+               direction = 'asc'
+               products = products.annotate(
                     lower_name=functions.Lower('product_name'))
 
-            # if sortkey == 'category':
-            #     sortkey == 'category__category_name'
-            #     print(f'sorted by category: {sortkey}')
+            if sortkey == 'name-desc':
+                sortkey = 'product_name'
+                direction = 'desc'
+                products = products.annotate(
+                    lower_name=functions.Lower('product_name'))
+            
+            if sortkey == 'price-asc':
+               sortkey = 'price'
+               direction = 'asc'
 
-            if 'direction' in self.request.GET:
-                direction = self.request.GET['direction']
+            if sortkey == 'price-desc':
+                sortkey = 'price'
+                direction = 'desc'
+
+
+            if sortkey == 'rating':
+                sortkey = 'rating'
+                direction = 'asc'
+                # products = products.annotate(
+                #     rating_int=int('products__rating'))
+
+            if 'direction':
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
 
                 products = products.order_by(sortkey)
-                print(products)
+                # print(products)
 
         # filter by category
         if 'category' in self.request.GET:
             categories = self.request.GET['category'].split(',')
-            print(f'Categories passed in the url: {categories}')
-            products = products.filter(category__category_name__in=categories)
-            categories = Category.objects.filter(category_name__in=categories)
-            print(f'Object:{categories}')
+            # print(f'Categories passed in the url: {categories}')
+            products = products.filter(category__slug__in=categories)
+            categories = Category.objects.filter(slug__in=categories)
+            # print(f'Object:{categories}')
+            # print(products)
 
         # filter by color
         if 'color' in self.request.GET:
             colors = self.request.GET['color'].split(',')
-            print(f'Colors passed in the url: {colors}')
-            products = products.filter(color__color__in=colors)
-            colors = Color.objects.filter(color__in=colors)
-            print(f'Object:{colors}')
+            # print(f'Colors passed in the url: {colors}')
+            products = products.filter(color__slug__in=colors)
+            colors = Color.objects.filter(slug__in=colors)
+            # print(f'Object:{colors}')
+            # print(products)
 
         # filter by size
         if 'size' in self.request.GET:
             sizes = self.request.GET['size'].split(',')
-            print(f'Colors passed in the url: {sizes}')
-            products = products.filter(size__size_short__in=sizes)
-            sizes = Size.objects.filter(size_short__in=sizes)
-            print(f'Object:{sizes}')
+            # print(f'Colors passed in the url: {sizes}')
+            products = products.filter(size__slug__in=sizes)
+            sizes = Size.objects.filter(slug__in=sizes)
+            # print(f'Object:{sizes}')
+            # print(products)
 
         if 'q' in self.request.GET:
             query = self.request.GET['q']
@@ -102,12 +129,16 @@ class ProductListView(ListView):
         category_list = Category.objects.all()
         color_list = Color.objects.all()
         size_list = Size.objects.all()
+        # print(colors)
+        # print(sizes)
 
         context = {
             'product_list': products,
             'search_term': query,
             'current_categories': categories,
             'current_sorting': current_sorting,
+            'current_colors': colors,
+            'current_sizes': sizes,
             'sort_type': sort_type,
             'sort_direction': sort_direction,
             'category_list': category_list,
@@ -141,7 +172,7 @@ class UpdateProductView(LoginRequiredMixin, UpdateView):
     form_class = CreateProductForm
     queryset = Product.objects.all()
     template_name = 'products/edit_product.html'
-    
+
     success_message = 'Product successfully edited.'
 
     def get_success_url(self):
