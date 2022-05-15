10604017e00d70180b8b818d411057ff14a8fdf9
Koko-66 <k.cisek@gmail.com>
2022-05-15 12:54:29 +0000

    Add 500 page and update error templates.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 """Home app views"""
-# from django.shortcuts import HttpResponse
+from django.shortcuts import render
 from django.views.generic import TemplateView
 # from django.contrib import messages
 
@@ -8,5 +8,12 @@ class HomeView(TemplateView):
     """Render home view"""
     template_name = 'home/index.html'
 
+
 def error_404(request, exception):
+    """Custom 404 error page"""
     return render(request, '404.html')
+
+
+def error_500(request):
+    """Custom 500 error page"""
+    return render(request, '500.html')
