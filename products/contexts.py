f4ded50b6fe5474bf5efde28bb85831eccd7a6dc
Koko-66 <k.cisek@gmail.com>
2022-05-23 16:19:50 +0000

    Fix rendering of stars in all products page.
    Add dictionary to hold stars per product and loop through this in the tempalte.

--- products/contexts.py
+++ products/contexts.py
@@ -3,13 +3,13 @@
 from math import modf
 from django.db.models import Avg
 from products.models import Product, Rating
-from django.db.models import Exists, OuterRef
 
 
 def get_ratings(request):
     """Get ratings for products"""
     products = Product.objects.all()
     avg_ratings = {}
+    star_ratings = {}
     latest_product_ratings = {}
     older_product_ratings = {}
     all_rev_users = {}
@@ -22,20 +22,21 @@ def get_ratings(request):
         if avg_rating:
             avg_rating = round(avg_rating * 2) / 2
             avg_ratings[product] = avg_rating
-
             h_star, f_stars = modf(avg_rating)
             stars = []
             count = 0
             while count < f_stars:
                 stars.append('star')
                 count += 1
+            if h_star:
+                stars.append('h_star')
+            star_ratings[product] = stars
+           
             # Get list of users who rated the product
             users_rev = []
             for rating in all_prod_ratings:
                 users_rev.append(rating.user)
                 all_rev_users[product] = users_rev
-
-        print(all_rev_users)
 
         latest_product_ratings[product] = Rating.objects.filter(product=product)[:3]
         older_product_ratings[product] = Rating.objects.filter(product=product)[4::]
@@ -48,5 +49,6 @@ def get_ratings(request):
         'stars': stars,
         'h_star': h_star,
         'all_rev_users': all_rev_users,
+        'star_ratings': star_ratings,
     }
     return context
