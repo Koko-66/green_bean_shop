2a29f9f43f3320e20731e6f85bb2ea8423a54076
Koko-66 <k.cisek@gmail.com>
2022-05-30 20:08:41 +0000

    Remove if from e-mail settings.

--- green_bean/settings.py
+++ green_bean/settings.py
@@ -126,20 +126,20 @@ AUTHENTICATION_BACKENDS = [
 SITE_ID = 1
 
 # Settings for email
-if os.path.isfile('env.py') and os.environ.get('DEVELOPMENT') == 'True':
-    print('This is development')
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = os.environ.get(
-        'DEVELOPMENT_FROM_EMAIL')
-else:
-    print('This is not in development')
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PSWD')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+# if os.path.isfile('env.py') and os.environ.get('DEVELOPMENT') == 'True':
+#     print('This is development')
+#     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#     DEFAULT_FROM_EMAIL = os.environ.get(
+#         'DEVELOPMENT_FROM_EMAIL')
+# else:
+#     print('This is not in development')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PSWD')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
