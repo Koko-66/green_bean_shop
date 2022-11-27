83583757774f228d66306ef6cb5aaac1ad9112d7
Koko-66 <k.cisek@gmail.com>
2022-11-27 20:37:22 +0000

    Add Render to allowed hosts.

--- green_bean/test_settings.py
+++ green_bean/test_settings.py
@@ -37,6 +37,11 @@ DEVELOPMENT = os.environ.get('DEVELOPMENT', 'False') == 'True'
 ALLOWED_HOSTS = ['green-bean-shop.herokuapp.com', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = [os.environ.get('CSRF_TRUSTED_ORIGINS')]
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Application definition
 
 INSTALLED_APPS = [
